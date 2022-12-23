e6d428ec5ee8170d1b7d1278acae66d006a9f54e
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-23 12:48:59 +0100

    Added Serie 12

---
+++ Serie12/Exercise/Program.fs
@@ -0,0 +1,12 @@
+﻿let isWoman (name: string) : bool =
+    match name with
+    | "Aline"
+    | "Cynthia" -> true
+    | _ -> false
+
+let people =
+    [ "Aline"; "Bernard"; "Cynthia" ]
+
+printfn $"There are women in the list of people: {List.exists isWoman people}"
+
+printfn $"There are only women in the list of people: {List.forall isWoman people}"
---
+++ Serie12/README.md
@@ -0,0 +1,3 @@
+# Séance d'exercices 12
+
+Les instructions pour la séance d'exercices se trouvent dans le fichier `instructions.pdf`.
